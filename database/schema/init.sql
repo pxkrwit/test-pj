47757f0bfa25691ef7d5a29661beb9423219cfdc
Korawit Rungthampaisan <201436386+pxkrwit@users.noreply.github.com>
2025-11-16 22:06:46 +0700

    update

--- database/schema/init.sql
+++ database/schema/init.sql
@@ -1,17 +1,16 @@
 CREATE DATABASE project_database;
 use project_database;
 CREATE TABLE study_plans (
-    id int(10) AUTO_INCREMEN PRIMARY KEY,
-    plan_code VARCHAR(2) UNIQUE CHECK (plan_code REGEXP '^[A-Za-z]{2}$'),
-    name_eng VARCHAR(60)  NOT NULL CHECK (name_eng <> ''),
-    name_th VARCHAR(100) NOT NULL Check (name_th <> '')
+    id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    planCode VARCHAR(2) UNIQUE CHECK (planCode REGEXP '^[A-Za-z]{2}$'),
+    nameEng VARCHAR(60)  NOT NULL CHECK (nameEng <> ''),
+    nameTh VARCHAR(100) NOT NULL Check (nameTh <> '')
 )CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 CREATE TABLE declared_plans (
-	student_id INT(11) NOT NULL,
-    plan_id INT NOT NULL,
-    created_at DATETIME,
-    updated_at DATETIME,
-    PRIMARY KEY (student_id, plan_id),
-    FOREIGN KEY (plan_id) REFERENCES study_plans(id)
+	studentId CHAR(11) CHECK (studentId REGEXP '^[0-9]{11}$') PRIMARY KEY,
+    planId INT NOT NULL,
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    FOREIGN KEY (planId) REFERENCES study_plans(id)
 )CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
--- database/seed/seed_data.sql
+++ database/seed/seed_data.sql
@@ -1,6 +1,6 @@
 USE project_database;
 
-INSERT INTO study_plans (plan_code, name_eng, name_th)
+INSERT INTO study_plans (planCode, nameEng, nameTh)
 VALUES
 ('FE','Frontend Developer','นักพัฒนาฟรอนเอนด์'),
 ('BE','Backend Developer','นักพัฒนาแบ็กเอนด์'),
@@ -11,3 +11,7 @@ VALUES
 ('DE','Data Engineer','วิศวกรข้อมูล'),
 ('DB','Database Administrator','ผู้ดูแลฐานข้อมูล'),
 ('UX','UX/UI Designer','นักออกแบบประสบการณ์ของผู้ใช้และส่วนต่อประสาน');
+
+INSERT INTO declared_plans VALUES 
+('67130500140',3,'2025-11-10 14:15:16','2025-11-10 17:18:19'),
+('67130500141',4,'2025-11-10 15:59:59','2025-11-10 15:59:59');
